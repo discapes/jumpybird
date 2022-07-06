d496645b4529f80f9b231a09fe71080bf7a13fe7
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-07-07 02:11:22 +0300

    build and dev configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["index.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
