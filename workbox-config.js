3079fb23dae41c1499e8fe603f55903bb98ab155
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-07-27 22:45:48 +0300

    pwa additions, works offline

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,15 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{js,css,ico,png,html,json}'
+		'**/*.{js,css,ico,png,html,json,jpg,ogg,mp3}'
 	],
 	swDest: 'public/sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	]
+	],
+	runtimeCaching: [{
+		handler: "NetworkFirst",
+		urlPattern: () => 1,
+	}]
 };
